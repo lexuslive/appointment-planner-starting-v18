e47030a8800de332b39e2b7976ad4999b73af524
Lex <lexus.sallahbrown@gmail.com>
2024-11-03 10:06:49 +0000

    Added app requirements to the funtion build

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email
+      }
+    ])
+  };
+
+  const addAppointment = (name, contact, date, time) => {
+    setContacts([
+      ...appointments,
+      {
+        name: name,
+        contact: contact,
+        date: date,
+        time: time
+      }
+    ])
+  };
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
